8b196e8458c04afc51a572e0e52475c679bd4e7a
arturborowy <artur.borowy@proexe.pl>
2019-07-10 19:34:08 +0200

    Add throwable and any log options to UltLog.

--- app/src/main/java/pl/arturborowy/logger/UltLog.kt
+++ app/src/main/java/pl/arturborowy/logger/UltLog.kt
@@ -7,19 +7,38 @@ import org.koin.core.parameter.parametersOf
 
 object UltLog : KoinComponent {
 
+    private const val DEFAULT_LOG_MESSAGE = "Empty log"
+
     lateinit var defaultTagSettings: TagSettings
 
     private val tagBuilder: TagBuilder
             by inject(parameters = { parametersOf(defaultTagSettings) })
 
-    fun e(msg: String,
+    fun e(msg: String? = DEFAULT_LOG_MESSAGE,
           withFileNme: Boolean? = null,
           withClassName: Boolean? = null,
           withMethodName: Boolean? = null,
-          withLineNumber: Boolean? = null) =
-            Log.e(tagBuilder.build(withFileNme,
-                    withClassName,
-                    withMethodName,
-                    withLineNumber), msg)
+          withLineNumber: Boolean? = null) {
+        Log.e(tagBuilder.build(withFileNme,
+                withClassName,
+                withMethodName,
+                withLineNumber), msg)
+    }
 
+    fun e(throwable: Throwable?,
+          extraMessage: String? = null) {
+        Log.e(tagBuilder.build(withFileNme = true,
+                withClassName = false,
+                withMethodName = false,
+                withLineNumber = false),
+                extraMessage,
+                throwable)
+    }
+
+    fun <AnyT> e(anything: AnyT?,
+                 withFileNme: Boolean? = null,
+                 withClassName: Boolean? = null,
+                 withMethodName: Boolean? = null,
+                 withLineNumber: Boolean? = null) =
+            e(anything.toString(), withFileNme, withClassName, withMethodName, withLineNumber)
 }
