9a044fe149404bd73216dc0a13a93938e82c62b3
arturborowy <artur.borowy@proexe.pl>
2019-07-13 20:12:58 +0200

    Create TagDataProvider interface.

---
+++ app/src/main/java/pl/arturborowy/logger/tag/dataprovider/TagData.kt
@@ -0,0 +1,6 @@
+package pl.arturborowy.logger.tag.dataprovider
+
+data class TagData(val fileName: String,
+                   val className: String,
+                   val methodName: String,
+                   val lineNumber: String)
---
+++ app/src/main/java/pl/arturborowy/logger/tag/dataprovider/TagDataProvider.kt
@@ -0,0 +1,6 @@
+package pl.arturborowy.logger.tag.dataprovider
+
+interface TagDataProvider {
+
+    fun getTagData() : TagData
+}
