7b9800cf8d54598edf98b060c7ea129e42fc70a3
arturborowy <artur.borowy@proexe.pl>
2019-07-14 11:46:16 +0200

    Create TagDataConverterTest.

---
+++ app/src/test/java/pl/arturborowy/TagDataConverterTest.kt
@@ -0,0 +1,33 @@
+package pl.arturborowy
+
+import org.junit.Assert
+import org.junit.Test
+import pl.arturborowy.logger.tag.dataprovider.TagData
+import pl.arturborowy.logger.tag.dataprovider.TagDataConverter
+
+class TagDataConverterTest {
+
+    private val givenClassName = "givenClassName"
+    private val givenFileName = "givenFileName"
+    private val givenMethodName = "givenMethodName"
+    private val givenLineNr = 12
+
+    private val tagDataConverter = TagDataConverter()
+
+    @Test
+    fun `fromStackTraceElement returns TagData with data from given stackTraceElement`() {
+        val givenStackTraceElement = StackTraceElement(givenClassName,
+                givenMethodName, givenFileName, givenLineNr)
+
+        val actualTagData = tagDataConverter.fromStackTraceElement(givenStackTraceElement)
+
+        assertTagDataHasGivenDataSet(actualTagData)
+    }
+
+    private fun assertTagDataHasGivenDataSet(actualTagData: TagData) {
+        Assert.assertEquals(givenClassName, actualTagData.className)
+        Assert.assertEquals(givenFileName, actualTagData.fileName)
+        Assert.assertEquals(givenMethodName, actualTagData.methodName)
+        Assert.assertEquals(givenLineNr, actualTagData.lineNumber.toInt())
+    }
+}
