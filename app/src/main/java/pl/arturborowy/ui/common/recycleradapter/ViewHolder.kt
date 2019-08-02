a99021c43c2c73a96c4f267e999033294c236ce3
Artur Borowy <arturborowy2137@gmail.com>
2019-08-03 00:49:17 +0200

    Remove boilerplate files.

--- app/src/main/java/pl/arturborowy/ui/common/recycleradapter/ViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package pl.arturborowy.ui.common.recycleradapter
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class ViewHolder<ItemT>(itemView: View, val bind: (item: ItemT, position: Int) -> Unit) :
-        RecyclerView.ViewHolder(itemView)
