328f73a084dab7abdae3c407cd4c4bd480223a69
Artur Borowy <arturborowy2137@gmail.com>
2019-08-08 00:04:15 +0200

    Cleanup ClassIgnorableStackTraceElementProvider with ClassIgnorableStackTraceElementProviderTest.

---
+++ ultimateloggermp/src/test/java/pl/arturborowy/ultimatelogger/tag/ClassIgnorableStackTraceElementProviderTest.kt
@@ -0,0 +1,70 @@
+package pl.arturborowy.ultimatelogger.tag
+
+import com.nhaarman.mockitokotlin2.given
+import com.nhaarman.mockitokotlin2.mock
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import pl.arturborowy.tagsettings.TagSettings
+import pl.arturborowy.ultimatelogger.data.TagSettingsRepository
+import pl.arturborowy.ultimatelogger.tag.dataprovider.stacktrace.ClassIgnorableStackTraceElementProvider
+import pl.arturborowy.ultimatelogger.tag.dataprovider.stacktrace.StackTraceFromThreadProvider
+import pl.arturborowy.ultimatelogger.util.TestData
+
+internal class ClassIgnorableStackTraceElementProviderTest {
+
+    private val mockStackTraceFromThreadProvider: StackTraceFromThreadProvider = mock()
+    private val mockTagSettingsRepository: TagSettingsRepository = mock()
+
+    private lateinit var classIgnorableStackTraceElementProvider:
+            ClassIgnorableStackTraceElementProvider
+
+    @Before
+    fun setUp() {
+        given(mockTagSettingsRepository.defaultTagSettings)
+                .willReturn(
+                        TagSettings(
+                                shouldLogFileNameAndLineNum = true,
+                                shouldLogClassName = true,
+                                shouldLogMethodName = true)
+                )
+
+        classIgnorableStackTraceElementProvider =
+                ClassIgnorableStackTraceElementProvider(
+                        mockStackTraceFromThreadProvider,
+                        mockTagSettingsRepository)
+    }
+
+    @Test
+    fun `getStackTraceElement() returns first element from stackTraceFromThreadProvider provide()`() {
+        given(mockStackTraceFromThreadProvider.provide()).willReturn(TestData.getStackTrace())
+
+        val expectedStackTraceElement = StackTraceElement("Class1", "Method1", "File1", 1)
+        val actualStackTraceElement =
+                classIgnorableStackTraceElementProvider.getStackTraceElement()
+
+        Assert.assertEquals(expectedStackTraceElement, actualStackTraceElement)
+    }
+
+    @Test
+    fun `getStackTraceElement() ignores every classes that should`() {
+        given(mockStackTraceFromThreadProvider.provide())
+                .willReturn(TestData.getStackTraceWithElementsShouldBeIgnored())
+
+        val expectedStackTraceElement = StackTraceElement("Class4", "Method4", "File4", 4)
+        val actualStackTraceElement =
+                classIgnorableStackTraceElementProvider.getStackTraceElement()
+
+        Assert.assertEquals(expectedStackTraceElement, actualStackTraceElement)
+    }
+
+    @Test
+    fun `getStackTraceElement() returns null if stackTraceFromThreadProvider provide() returned null`() {
+        given(mockStackTraceFromThreadProvider.provide()).willReturn(null)
+
+        val actualStackTraceElement =
+                classIgnorableStackTraceElementProvider.getStackTraceElement()
+
+        Assert.assertEquals(null, actualStackTraceElement)
+    }
+}
